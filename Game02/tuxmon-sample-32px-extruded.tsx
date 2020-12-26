7dbed859fcb00df5b34d711cb590264f9c32f46b
Dr <rcolasanti@bournemouth.ac.uk>
2020-12-26 17:17:31 +0000

    new backgrounds and main charater

--- Game02/tuxmon-sample-32px-extruded.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="tuxmon-sample-32px-extruded" tilewidth="32" tileheight="32" tilecount="775" columns="25">
- <image source="assets/tuxmon-sample-32px-extruded.png" width="816" height="1020"/>
-</tileset>
