9301ba9375befb9844123cb2e7cdd3ebedffb486
Dr <rcolasanti@bournemouth.ac.uk>
2021-01-01 14:58:50 +0000

    update

--- RoswellRunner/js/game.js
+++ RoswellRunner/js/game.js
@@ -94,12 +94,11 @@ class Alien extends Player {
         this.live = true
     }
     update(cursors) {
-        if (this.x > 5100) {
+        if (this.x > this.scene.arenaWidth) {
             this.setX(5101)
-            this.setVelocityX(0)
             this.anims.play('stop', true);
             this.scene.sound.stopAll();
-            this.scene.stop()
+            this.scene.scene.stop('Game')
             this.scene.scene.start('End');
         } else {
             if (this.live) {
@@ -115,14 +114,11 @@ class Alien extends Player {
                     this.scene.restartGame()
                 }
             }
-            if (this.x > 5200) {
-                this.setX(5199)
-                this.setVelocityX(0)
-            }
         }
     }
     die() {
         this.live = false
+        this.body.checkCollision.none = true;
         this.anims.play('dead', true);
         this.scene.sound.stopAll();
         this.scene.oopsSound.play()
@@ -154,41 +150,11 @@ class StartScene extends Phaser.Scene {
     }
     create() {
         this.add.tileSprite(100, 80, 123, 130, "ufo");
-        this.add.text(230, 20, 'Welcome to Roswell runner ', {
+        content = ['Welcome to Roswell runner ', 'One of the little bods from area 51', 'Is trying to escape to route 66', "", 'Our plucky alien will need some bus fare though !', 'Help your new chum avoid dangers and collect coins', "", 'Give your alien a boost with your spacecraft ', 'Move your craft with arrow keys', "But don't let your alien chum fall too far !", "", "", 'Click space key to start']
+        this.add.text(60, 60, content, {
             fontFamily: 'Luckiest Guy',
-            fontSize: '30px'
-        });
-        this.add.text(180, 60, 'One of the little bods from area 51', {
-            fontFamily: 'Luckiest Guy',
-            fontSize: '30px'
-        });
-        this.add.text(220, 100, 'Is trying to escape to route 66', {
-            fontFamily: 'Luckiest Guy',
-            fontSize: '30px'
-        });
-        this.add.text(90, 140, 'Our plucky alien will need some bus fare though !', {
-            fontFamily: 'Luckiest Guy',
-            fontSize: '30px'
-        });
-        this.add.text(60, 180, 'Help your new chum avoid dangers and collect coins', {
-            fontFamily: 'Luckiest Guy',
-            fontSize: '30px'
-        });
-        this.add.text(110, 230, 'Give your alien a boost with your spacecraft ', {
-            fontFamily: 'Luckiest Guy',
-            fontSize: '30px'
-        });
-        this.add.text(190, 270, 'Move your craft with arrow keys', {
-            fontFamily: 'Luckiest Guy',
-            fontSize: '30px'
-        });
-        this.add.text(150, 320, "But don't let your alien chum fall too far !", {
-            fontFamily: 'Luckiest Guy',
-            fontSize: '30px'
-        });
-        this.add.text(260, 380, 'Click space key to start', {
-            fontFamily: 'Luckiest Guy',
-            fontSize: '30px'
+            fontSize: '30px',
+            align: "center"
         });
         this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         this.spaceKey.on('down', function (key, event) {
@@ -197,16 +163,18 @@ class StartScene extends Phaser.Scene {
 
     }
 }
+
+
 class EndScene extends Phaser.Scene {
     constructor() {
         super("End");
     }
     create() {
-      const text =  this.add.text(370, 120, 'Game over !', {
+        const text = this.add.text(370, 120, 'Game over !', {
             fontFamily: 'Luckiest Guy',
             fontSize: '30px'
         });
-     const text2 =    this.add.text(280, 220, 'Click space key to restart', {
+        const text2 = this.add.text(280, 220, 'Click space key to restart', {
             fontFamily: 'Luckiest Guy',
             fontSize: '30px'
         });
@@ -222,134 +190,133 @@ class GameScene extends Phaser.Scene {
         super("Game");
     }
     preload() {
-            this.load.tilemapTiledJSON('tilemap', 'assets/platform1.json');
-            this.load.image('tiles', 'assets/tilesheet_complete.png');
-            this.load.image('coin', 'assets/coinGold.png')
-            this.load.spritesheet(PLAYER1, 'assets/p3_walk.png', {
-                frameWidth: 66,
-                frameHeight: 93
-            });
-            this.load.spritesheet(PLAYER2, 'assets/hover.png', {
-                frameWidth: 123,
-                frameHeight: 72
-            });
-            this.load.spritesheet(ENEMIE1, 'assets/fly_fly.png', {
-                frameWidth: 66,
-                frameHeight: 43
-            });
-            this.load.image('sky', 'assets/sky.png');
-            this.load.image('rocks', 'assets/Rocks.png');
-            this.load.image('ground', 'assets/Layer1.png');
-            this.load.audio('bgmusic', ['assets/Arcade-game-music-loop.mp3']);
-            this.load.audio('bounce', ['assets/forceField_001.mp3']);
-            this.load.audio('coin', ['assets/impactGlass_medium_003.mp3']);
-            this.load.audio('oops', ['assets/secret.mp3']);
-    
-     
+        this.load.tilemapTiledJSON('tilemap', 'assets/platform1.json');
+        this.load.image('tiles', 'assets/tilesheet_complete.png');
+        this.load.image('coin', 'assets/coinGold.png')
+        this.load.spritesheet(PLAYER1, 'assets/p3_walk.png', {
+            frameWidth: 66,
+            frameHeight: 93
+        });
+        this.load.spritesheet(PLAYER2, 'assets/hover.png', {
+            frameWidth: 123,
+            frameHeight: 72
+        });
+        this.load.spritesheet(ENEMIE1, 'assets/fly_fly.png', {
+            frameWidth: 66,
+            frameHeight: 43
+        });
+        this.load.image('sky', 'assets/sky.png');
+        this.load.image('rocks', 'assets/Rocks.png');
+        this.load.image('ground', 'assets/Layer1.png');
+        this.load.audio('bgmusic', ['assets/Arcade-game-music-loop.mp3']);
+        this.load.audio('bounce', ['assets/forceField_001.mp3']);
+        this.load.audio('coin', ['assets/impactGlass_medium_003.mp3']);
+        this.load.audio('oops', ['assets/secret.mp3']);
+
+
     }
     create() {
+
+        this.bg_1 = this.add.tileSprite(-400, -30, game.config.width + 800, game.config.height, "sky");
+        this.bg_1.setOrigin(0, 0);
+        this.bg_1.setScrollFactor(0.05)
+
+        this.bg_2 = this.add.tileSprite(0, -10, this.arenaWidth, game.config.height, "rocks");
+        this.bg_2.setOrigin(0, 0);
+        this.bg_2.setScrollFactor(0.5);
+
+        this.bg_3 = this.add.tileSprite(200, 150, this.arenaWidth, game.config.height, "rocks");
+        this.bg_3.setOrigin(0, 0);
+        this.bg_3.setScrollFactor(0.7);
+
         
-            this.bg_1 = this.add.tileSprite(-400, -30, game.config.width + 800, game.config.height, "sky");
-            this.bg_1.setOrigin(0, 0);
-            this.bg_1.setScrollFactor(0.05)
-    
-            this.bg_2 = this.add.tileSprite(0, -10, 5000, game.config.height, "rocks");
-            this.bg_2.setOrigin(0, 0);
-            this.bg_2.setScrollFactor(0.5);
-    
-            this.bg_3 = this.add.tileSprite(200, 150, 5000, game.config.height, "rocks");
-            this.bg_3.setOrigin(0, 0);
-            this.bg_3.setScrollFactor(0.7);
-    
-            this.player1 = new Alien(this, 120, 420, PLAYER1)
-            this.player2 = new Ship(this, 420, 420, PLAYER2)
-            this.enemies = this.add.group()
-            this.enemies.add(new Enemie(this, 4500, 140, ENEMIE1))
-            this.enemies.add(new Enemie(this, 3400, 400, ENEMIE1))
-    
-            const map = this.make.tilemap({
-                key: 'tilemap'
-            })
-    
-    
-            const tileset = map.addTilesetImage('tilesheet_complete', 'tiles')
-            this.colide = map.createLayer('ground', tileset, 0, 0)
-            this.colide.setCollisionByProperty({
-                colide: true
-            })
-    
-            const coins = map.getObjectLayer('Object Layer 1')
-            const gcoins = this.physics.add.staticGroup()
-            for (var c in coins.objects) {
-                gcoins.create(coins.objects[c].x + 32, coins.objects[c].y, 'coin');
+        const map = this.make.tilemap({
+            key: 'tilemap'
+        })
+        this.arenaWidth = map.widthInPixels
+        this.arenaHeight = map.heightInPixels
+        const tileset = map.addTilesetImage('tilesheet_complete', 'tiles')
+        this.colide = map.createLayer('ground', tileset, 0, 0)
+        this.colide.setCollisionByProperty({
+            colide: true
+        })
+
+        this.player1 = new Alien(this, 120, 420, PLAYER1)
+        this.player2 = new Ship(this, 420, 420, PLAYER2)
+        this.enemies = this.add.group()
+        this.enemies.add(new Enemie(this, 4500, 140, ENEMIE1))
+        this.enemies.add(new Enemie(this, 3400, 400, ENEMIE1))
+
+
+
+
+        const coins = map.getObjectLayer('Object Layer 1')
+        const gcoins = this.physics.add.staticGroup()
+        for (var c in coins.objects) {
+            gcoins.create(coins.objects[c].x + 32, coins.objects[c].y, 'coin');
+        }
+
+        this.bg_4 = this.add.tileSprite(0, 490, this.arenaWidth*1.5, game.config.height, "ground");
+        this.bg_4.setOrigin(0, 0);
+        this.bg_4.setScrollFactor(1.5);
+
+        this.cameras.main.startFollow(this.player2, true);
+        this.cameras.main.setBounds(0, 0, this.colide.displayWidth, this.colide.displayHeight);
+
+        this.physics.add.collider(this.player1, this.colide, function (player, tile) {
+            player.hitTile(tile)
+        })
+        console.log(this.player1);
+        this.physics.add.collider(this.player2, this.colide)
+        this.physics.add.overlap(this.player1, this.player2, function (ob1, ob2) {
+            if (ob1.live) {
+                gameScene.bounceSound.play()
+                ob1.setVelocityY(-260)
             }
-    
-            this.bg_4 = this.add.tileSprite(0, 490, 14000, game.config.height, "ground");
-            this.bg_4.setOrigin(0, 0);
-            this.bg_4.setScrollFactor(1.5);
-    
-            this.cameras.main.startFollow(this.player2, true);
-            this.cameras.main.setBounds(0, 0, this.colide.displayWidth, this.colide.displayHeight);
-    
-            this.physics.add.collider(this.player1, this.colide, function (player, tile) {
-                player.hitTile(tile)
-            })
-            this.physics.add.collider(this.player2, this.colide)
-            this.physics.add.overlap(this.player1, this.player2, function (ob1, ob2) {
-                if (ob1.live) {
-                    gameScene.bounceSound.play()
-                    ob1.setVelocityY(-260)
-                }
-            })
-            this.physics.add.overlap(this.player1, gcoins, function collectStar(player, star) {
-                gameScene.coinSound.play()
-                gameScene.score++
-                star.disableBody(true, true);
-            })
-            this.physics.add.overlap(this.player1, this.enemies, function (player, enemie) {
-                player.die();
-            })
-            this.player1.addAnim('right', [0, 1, 2, 3, 4])
-            this.player1.addAnim('stop', [0])
-            this.player1.addAnim('dead', [12])
-            this.player2.addAnim('hover', [0, 1, 2], 10)
-    
-            this.player1.anims.play('right', true);
-            this.player2.anims.play('hover', true);
-    
-    
-    
-            //  Input Events
-            this.cursors = this.input.keyboard.createCursorKeys();
-    
-    
-            if (DEBUG) {
-                const debugGraphics = this.add.graphics().setAlpha(0.75);
-                this.colide.renderDebug(debugGraphics, {
-                    tileColor: null,
-                    collidingTileColor: new Phaser.Display.Color(244, 134, 48, 255),
-                    faceColor: new Phaser.Display.Color(40, 39, 37, 255)
-                });
-            }
-            var music = this.sound.add('bgmusic')
-             music.play();
-            this.bounceSound = this.sound.add('bounce')
-            this.coinSound = this.sound.add('coin')
-            this.oopsSound = this.sound.add('oops')
-            this.score = 0
-            this.text = this.add.text(20, 20, 'Score: ' + this.score, {
-                fontFamily: 'Luckiest Guy',
-                color: '#5E35B1',
-                fontSize: '30px'
+        })
+        this.physics.add.overlap(this.player1, gcoins, function collectStar(player, star) {
+            gameScene.coinSound.play()
+            gameScene.score++
+            star.disableBody(true, true);
+        })
+        this.physics.add.overlap(this.player1, this.enemies, function (player, enemie) {
+            player.die();
+        })
+        this.player1.addAnim('right', [0, 1, 2, 3, 4])
+        this.player1.addAnim('stop', [0])
+        this.player1.addAnim('dead', [12])
+        this.player2.addAnim('hover', [0, 1, 2], 10)
+
+        this.player1.anims.play('right', true);
+        this.player2.anims.play('hover', true);
+
+
+
+        //  Input Events
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+
+        if (DEBUG) {
+            const debugGraphics = this.add.graphics().setAlpha(0.75);
+            this.colide.renderDebug(debugGraphics, {
+                tileColor: null,
+                collidingTileColor: new Phaser.Display.Color(244, 134, 48, 255),
+                faceColor: new Phaser.Display.Color(40, 39, 37, 255)
             });
-            this.text.setScrollFactor(0.0);
-            // this.minimap = this.cameras.add(20, 300, 400, 100).setZoom(0.2).setName('mini');
-            // this.minimap.setBackgroundColor(0x002244);
-            // this.minimap.scrollX = 0;
-            // this.minimap.scrollY = 0;
-           
-    
-        
+        }
+        var music = this.sound.add('bgmusic')
+        music.play();
+        this.bounceSound = this.sound.add('bounce')
+        this.coinSound = this.sound.add('coin')
+        this.oopsSound = this.sound.add('oops')
+        this.score = 0
+        this.text = this.add.text(20, 20, 'Score: ' + this.score, {
+            fontFamily: 'Luckiest Guy',
+            color: '#5E35B1',
+            fontSize: '30px'
+        });
+        this.text.setScrollFactor(0.0);
+
     }
 
     update() {
@@ -362,7 +329,7 @@ class GameScene extends Phaser.Scene {
     }
     restartGame() {
         this.registry.destroy() // destroy registry
-        this.events.off()// disable all active events
+        this.events.off() // disable all active events
         this.sound.stopAll();
         this.scene.start('End');
     }
@@ -370,7 +337,7 @@ class GameScene extends Phaser.Scene {
 
 
 const gameScene = new GameScene();
-let startScene =new StartScene();
+let startScene = new StartScene();
 let endScene = new EndScene();
 
 var config = {
